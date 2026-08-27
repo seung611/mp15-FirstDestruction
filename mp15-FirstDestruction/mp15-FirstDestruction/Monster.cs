5c3e0113218942a73497c570507ca1d62401949f
Zebi <einz.yume@gmail.com>
2026-08-27 16:00:13 +0900

    Feat : 몬스터 수정
    Health 프로퍼티 수정
    생성자 추가
    TakeDamage 할당

--- mp15-FirstDestruction/mp15-FirstDestruction/Monster.cs
+++ mp15-FirstDestruction/mp15-FirstDestruction/Monster.cs
@@ -2,10 +2,16 @@
 
 public class Monster : IDamageable
 {
-    public int Health { get; set; }
+    public int Health { get; protected set; }
     public string Name;
+    Monster(int health, string name)
+    {
+        Health = health;
+        Name = name;
+    }
     public void TakeDamage(int damage)
     {
-        
+        Health -= damage;
+        Console.WriteLine($"{Name}가 {damage}데미지를 입었습니다");
     }
 }
