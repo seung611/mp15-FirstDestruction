64b697fc9667864cf207311755a6b55f2aaf6c60
Rocket22 <ldgnl22@gmail.com>
2026-08-27 14:40:45 +0900

    [Feat] Item클래스에 배열변수와 Add 함수 생성

--- mp15-FirstDestruction/mp15-FirstDestruction/Item.cs
+++ mp15-FirstDestruction/mp15-FirstDestruction/Item.cs
@@ -3,11 +3,21 @@
 public class Item
 {
     private string name;
+    Item[] items = new Item[10];
+    private int count = 0;
+    
+    public int Count { get { return count; } };
     
     public string Name { get; set; }
 
     public Item(string name)
     {
         Name = name;
+    }
+
+    public void Add(Item item)
+    {
+        items[count] = item;
+        count++;
     }
 }
